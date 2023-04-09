d2b585fcfc1791a41c53aa827233c8b1e5377bf1
danielcristho <pepuhodaniel93.learn@gmail.com>
2023-04-09 12:52:29 +0700

    fix(laravel):deleting chirps

--- laravel/chirper/app/Models/Chirp.php
+++ laravel/chirper/app/Models/Chirp.php
@@ -5,6 +5,7 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
 use Illuminate\Database\Eloquent\Model;
+use App\Events\ChirpCreated;
 
 class Chirp extends Model
 {
@@ -17,4 +18,8 @@ class Chirp extends Model
     {
         return $this->belongsTo(User::class);
     }
+
+    protected $dispatchesEvents = [
+        'created' => ChirpCreated::class,
+    ];
 }
