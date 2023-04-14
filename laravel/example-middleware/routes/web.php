de742e7cb968e0e8b0e64370e924b1ce1a75948c
danielcristho <pepuhodaniel93.learn@gmail.com>
2023-04-14 13:14:35 +0700

    feat: add user and create migrations

--- laravel/example-middleware/routes/web.php
+++ laravel/example-middleware/routes/web.php
@@ -13,6 +13,11 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/test-user', [App\Http\Controllers\HomeController::class, 'index'])->middleware(['auth', 'is-active']);
+// Route::get('/', function () {
+//     return view('welcome');
+// });
+
+Auth::routes();
+
+Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
